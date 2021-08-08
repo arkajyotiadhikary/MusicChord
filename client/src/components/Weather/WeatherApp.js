7e59d239466f736e1e100aed61764c3051a4f50b
arka <arkajyotiadhikary15@gmail.com>
2021-08-09 03:12:07 +0530

    api error solve

--- client/src/components/Weather/WeatherApp.js
+++ client/src/components/Weather/WeatherApp.js
@@ -1,23 +1,39 @@
 import React, { useState, useEffect } from "react";
 import WeatherBackground from "./WeatherBackground";
 import "./WeatherApp.css";
+import Axios from "axios";
+import axios from "axios";
 
 const Api = {
-  key: "",
-  base: `api.openweathermap.org/data/2.5/weather?q=Guwahati&appid=d2311387e5b14755e3ec508375660f94`,
+  key: "d2311387e5b14755e3ec508375660f94",
+  base: "http://api.openweathermap.org/data/2.5/weather?q=Guwahati&appid=",
 };
 
 const WeatherApp = () => {
   const [weather, setWeather] = useState({});
 
+  let time = new Date();
+  let min = time.getMinutes();
+
+  setTimeout(() => {
+    time = new Date();
+    min = time.getMinutes();
+  }, 1000);
+
+  console.log(min);
   useEffect(() => {
     const getWeather = async () => {
-      let data = await fetch(Api.key);
-      await setWeather(data.json());
-      await console.log(weather);
+      try {
+        const response = await axios.get(Api.base + Api.key);
+        setWeather(response);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
     };
     getWeather();
-  }, [Api.key]);
+    console.log("Updated");
+  }, [min]);
 
   return <h1>Weather</h1>;
 };
