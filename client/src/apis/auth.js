56e292d4483ceb5a434937bd3219e3c0bffd8bfe
Rituraj <riturajd265@gmail.com>
2021-08-15 21:11:02 +0530

    auth encoding and jwt setup

--- client/src/apis/auth.js
+++ client/src/apis/auth.js
@@ -3,20 +3,11 @@ import axios from "axios";
 const endpoint = "http://localhost:8000";
 
 const signUp = async (formData) => {
-    const registeredUser = await axios.post(
-        `${endpoint}/auth/signup`,
-        formData
-    );
-    console.log(registeredUser);
-};
-
-const signIn = async (formData) => {
     try {
-        const signInDetails = await axios.post(
-            `${endpoint}/auth/signin`,
+        const registeredUser = await axios.post(
+            `${endpoint}/auth/signup`,
             formData
         );
-        console.log(signInDetails);
     } catch (error) {
         const err = error.response;
         if (err.status === 400) {
@@ -26,4 +17,42 @@ const signIn = async (formData) => {
     }
 };
 
-export { signUp, signIn };
+const signIn = async (formData) => {
+    try {
+        const signInDetails = await axios.post(
+            `${endpoint}/auth/signin`,
+            formData
+        );
+        localStorage.setItem("token", signInDetails.data.token);
+    } catch (error) {
+        const err = error.response;
+        if (err.status === 400) {
+            console.log(err.data.msg);
+        }
+        console.log(err);
+    }
+};
+
+const loadUser = async () => {
+    const token = localStorage.getItem("token");
+
+    const body = {
+        token,
+    };
+
+    try {
+        const signInDetails = await axios.post(
+            `${endpoint}/auth/loaduser`,
+            body
+        );
+        console.log(signInDetails);
+    } catch (error) {
+        const err = error.response;
+        if (err.status === 400) {
+            console.log(err.data.msg);
+        }
+        console.log(error);
+    }
+};
+
+export { signUp, signIn, loadUser };
