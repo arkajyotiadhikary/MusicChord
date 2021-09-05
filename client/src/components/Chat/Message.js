48f11111aea960c4b20728ca7b5b565b3e9d65d7
The Bug <66660068+arkajyotiadhikary@users.noreply.github.com>
2021-09-05 11:44:02 +0530

    name along with message

--- client/src/components/Chat/Message.js
+++ client/src/components/Chat/Message.js
@@ -1,5 +1,5 @@
 const Message = ({ chatData }) => {
-    const { type, message } = chatData;
+    const { type, message, _data } = chatData;
 
     return (
         <div className={type === "selfMsg" ? "answer left" : "answer right"}>
@@ -10,7 +10,7 @@ const Message = ({ chatData }) => {
                 />
                 <div className="status offline"></div>
             </div>
-            <div className="name">Alexander Herthic</div>
+            <div className="name">{_data.username}</div>
             <div className="text">{message}</div>
             <div className="time">5 min ago</div>
         </div>
