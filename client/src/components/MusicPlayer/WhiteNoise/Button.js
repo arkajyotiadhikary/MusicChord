123fabe158fa245f6eb2e1fca2372b40de61824a
The Bug <66660068+arkajyotiadhikary@users.noreply.github.com>
2021-09-28 19:54:12 +0530

    white-noise-color

--- client/src/components/MusicPlayer/WhiteNoise/Button.js
+++ client/src/components/MusicPlayer/WhiteNoise/Button.js
@@ -1,8 +1,20 @@
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Button = (prop) => {
+    const [useBtn, setUseBtn] = useState(false);
+    const handleClick = () => {
+        useBtn ? setUseBtn(false) : setUseBtn(true);
+    };
+
     return (
-        <button className="btn btn-light shadow rounded-circle" type="Submit">
+        <button
+            className={`btn btn-light shadow ${
+                useBtn ? "bg-primary text-white" : "bg-white text-primary"
+            } rounded-circle`}
+            type="Submit"
+            onClick={handleClick}
+        >
             <FontAwesomeIcon icon={prop.font} />
         </button>
     );
